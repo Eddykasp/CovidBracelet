293a33694d01441f56615889371b723c7c65c2fb
Viktor <viktor.s.h@web.de>
2021-01-09 17:23:51 +0100

    Add base_time and time calculation for current epoch time.

--- src/current_time_service.c
+++ src/current_time_service.c
@@ -4,6 +4,8 @@
 #include <bluetooth/hci.h>
 #include <bluetooth/uuid.h>
 
+#include "current_time_service.h"
+
 typedef struct
 {
     uint16_t year;         // 1582-9999
@@ -19,6 +21,11 @@ typedef struct
 } date_time;
 
 date_time time = {2021, 1, 1, 12, 25, 25, 1, 1, 0};
+
+// Epoch time 01.01.2000 00:00:00 H
+uint32_t base_time        = 946684800;
+uint32_t last_time_update = 0;
+uint32_t current_time     = 0;
 
 static ssize_t read_time(
     struct bt_conn* conn,
@@ -42,6 +49,21 @@ static ssize_t write_time(
     return len;
 }
 
+static ssize_t write_time_epoch(
+    struct bt_conn* conn,
+    const struct bt_gatt_attr* attr,
+    const void* buf,
+    uint16_t len,
+    uint16_t offset,
+    uint8_t flags)
+{
+    memcpy(&last_time_update, buf, sizeof(uint32_t));
+    printk("current time is: %d \n", last_time_update);
+    current_time = last_time_update - base_time;
+    printk("current time is: %d \n", current_time);
+    return len;
+}
+
 BT_GATT_SERVICE_DEFINE(
     cts,
     BT_GATT_PRIMARY_SERVICE(BT_UUID_CTS),
@@ -58,4 +80,11 @@ BT_GATT_SERVICE_DEFINE(
         BT_GATT_PERM_READ,
         read_time,
         NULL,
+        NULL),
+    BT_GATT_CHARACTERISTIC(
+        BT_UUID_ENS_SETTINGS_CTE,
+        BT_GATT_CHRC_WRITE,
+        BT_GATT_PERM_WRITE,
+        NULL,
+        write_time_epoch,
         NULL));
--- src/temporary_key_list.c
+++ src/temporary_key_list.c
@@ -1,4 +1,5 @@
 #include "temporary_key_list.h"
+#include "current_time_service.h"
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/uuid.h>
 
@@ -17,5 +18,5 @@ void get_random_uuid(uint8_t* key)
 void get_temporary_advertisement_data(uint8_t* key, uint32_t* time)
 {
     get_random_uuid(key);
-    *time = k_uptime_get_32();
+    *time = current_time + k_uptime_get_32();
 }
