3ef2d4af1235f3cffa62f3b6eef56496c1708e42
Viktor <viktor.s.h@web.de>
2021-01-11 18:36:53 +0100

    Add RACP code-definitions

--- src/temp_key_api.h
+++ src/temp_key_api.h
@@ -3,9 +3,9 @@
 #include "temporary_key_list.h"
 
 // gets recent keys from ENS system
-temp_key_list* get_temp_key_list();
+temp_key_list_t* get_temp_key_list();
 
 // replaces ENS keys with new keys
-void set_temp_keys(temp_key_list* new_keys);
+void set_temp_keys(temp_key_list_t* new_keys);
 
 #endif
