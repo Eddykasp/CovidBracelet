a34a437c6e3b7f5786c950c0f1aec04b87eb0471
Viktor <viktor.s.h@web.de>
2021-03-25 15:50:50 +0100

    remove old date_time function

--- src/current_time_service.c
+++ src/current_time_service.c
@@ -8,20 +8,6 @@
 #include "ens_records_api.h"
 #include "temporary_key_list.h"
 
-typedef struct
-{
-    uint16_t year;         // 1582-9999
-    uint8_t month;         // 1-12
-    uint8_t day;           // 1-31
-    uint8_t hours;         // 0-23
-    uint8_t minutes;       // 0-59
-    uint8_t seconds;       // 0-59
-    uint8_t day_of_week;   // 1-7(monday-Sunday)
-    uint8_t fractions256;  // 0-255 (number of 1/256 fractions of a second)
-    uint8_t adjust_reason; // 0 = manual update, 1 = external reference time update, 2 = change
-                           // timezone, 3 = change of DST
-} date_time;
-
 date_time time = {2021, 1, 1, 12, 25, 25, 1, 1, 0};
 
 // Epoch time 01.01.2000 00:00:00 H
--- src/current_time_service.h
+++ src/current_time_service.h
@@ -2,6 +2,19 @@
 #define CURRENT_TIME_SERVICE_H
 #include <bluetooth/bluetooth.h>
 #include <bluetooth/uuid.h>
+typedef struct
+{
+    uint16_t year;         // 1582-9999
+    uint8_t month;         // 1-12
+    uint8_t day;           // 1-31
+    uint8_t hours;         // 0-23
+    uint8_t minutes;       // 0-59
+    uint8_t seconds;       // 0-59
+    uint8_t day_of_week;   // 1-7(monday-Sunday)
+    uint8_t fractions256;  // 0-255 (number of 1/256 fractions of a second)
+    uint8_t adjust_reason; // 0 = manual update, 1 = external reference time update, 2 = change
+                           // timezone, 3 = change of DST
+} date_time;
 
 // Epoch time
 extern uint32_t base_time;
